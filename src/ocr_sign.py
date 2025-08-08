d57d0365f617d36e7063b5cdc605d1c6cf964345
shinjaehyeon <pumpkinalpha996@gmail.com>
2025-08-08 13:21:59 +0900

    출력 코드 작성

--- src/ocr_sign.py
+++ src/ocr_sign.py
@@ -1,14 +1,13 @@
 import numpy as np
-import cv2
-import easyocr
-import os
+import cv2, os, easyocr
+import matplotlib.pyplot as plt
 
 path = '../img/'
 name = 'signs.jpg'
 
 img = cv2.imread(os.path.join(path+name))
 
-reader = easyocr.Reader(['ko', 'en'], gpu=False)
+reader = easyocr.Reader(['ko'], gpu=False)
 result = reader.readtext(os.path.join(path+name))
 
 for bbox, text, conf in result:
@@ -21,4 +20,13 @@ for bbox, text, conf in result:
     y = min(ys)
     w = abs(max(xs)-min(xs))
     h = abs(max(ys)-min(ys))
-    print(x,y,w,h)
+    # print(x,y,w,h)
+    cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+    if type(text) is int:
+        cv2.putText(img, f'제한속도: {text}', (x, y-30), cv2.FONT_HERSHEY_PLAIN, 2, (0,255,0), 2)
+    else: print(f'전방 {text}입니다.')
+
+plt.figure(figsize=(8,8))
+plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+plt.axis('off')
+plt.show()
