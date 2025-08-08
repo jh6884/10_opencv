5e14acc8bd5add5fab7bc1e76b4c4c0142363cdd
shinjaehyeon <pumpkinalpha996@gmail.com>
2025-08-08 13:13:19 +0900

    좌표값, 폭, 높이 계산

--- src/ocr_sign.py
+++ src/ocr_sign.py
@@ -1,16 +1,24 @@
 import numpy as np
 import cv2
 import easyocr
+import os
 
 path = '../img/'
 name = 'signs.jpg'
 
-img = cv2.imread(path+name)
+img = cv2.imread(os.path.join(path+name))
 
 reader = easyocr.Reader(['ko', 'en'], gpu=False)
-result = reader.readtext(img)
+result = reader.readtext(os.path.join(path+name))
 
 for bbox, text, conf in result:
-    print(bbox)
-
-print(result)
+    xs = []
+    ys = []
+    for i in bbox:
+        xs.append(i[0])
+        ys.append(i[1])
+    x = min(xs)
+    y = min(ys)
+    w = abs(max(xs)-min(xs))
+    h = abs(max(ys)-min(ys))
+    print(x,y,w,h)
