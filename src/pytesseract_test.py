08fdad8194c054bd32ba20d52864b75693be0dac
shinjaehyeon <pumpkinalpha996@gmail.com>
2025-08-08 12:13:20 +0900

    Update pytesseract_test.py

--- src/pytesseract_test.py
+++ src/pytesseract_test.py
@@ -2,19 +2,18 @@ import pytesseract
 import cv2
 import matplotlib.pyplot as plt
 
+tesseract_cmd = 'C:\Program Files\Tesseract-OCR\tesseract.exe'
 img_path = '../img/chinese_tra.jpg'
 # img_path = '../img/'
 
 img = cv2.imread(img_path)
+img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 # 이미지 불러오기
-# plt.figure(figsize = (8,8))
-# plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-# plt.show()
 
 # 이미지 텍스트 인식
-data = pytesseract.image_to_data(img_path, lang='chi_tra+eng', output_type=pytesseract.Output.DICT)
-print(data)
+string = pytesseract.image_to_string(img_rgb)
+print(string)
 
 # 인식된 텍스트 확인해보기
 THRESHOLD = 0.5
